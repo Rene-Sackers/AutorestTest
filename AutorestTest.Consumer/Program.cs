9dce6cdf466a0f3681e6028f9ab183fd0c5a0a6e
René Sackers <rene.sackers@live.com>
2022-07-27 11:18:27 +0200

    Fix client code

--- AutorestTest.Consumer/Program.cs
+++ AutorestTest.Consumer/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics;
 using AutorestTest.Client;
 
-var client = new AutorestTestApiClient(new (""), new());
+var client = new AutorestTestApiClient(new ("https://localhost:7164"), new());
+// or just `AddResponseData result = ...`
 Result<AddResponseData> result = await client.AddAsync(new AddRequestData()
 {
 	Value1 = 1,
 	Value2 = 2
 });
-Debug.Assert(result.Value == 3);
+Debug.Assert(result.Value.Sum == 3);
